8df9d91cf407fdb226516d93d3fd0ad1f8b9df33
Gnuxie <Gnuxie@protonmail.com>
2019-01-02 05:40:02 +0000

    fixed relative paths on definitions index

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -8,3 +8,14 @@
 (defmethod staple:compile-source ((document pathname) (page staple:definitions-index-page))
   (staple:compile-source document :markdown))
 |#
+
+(defun strip-to-relative (some-path-string)
+  (let ((chop-point (search "cl-matrix" some-path-string)))
+    (format *standard-output* "~a~%" (subseq some-path-string (+ chop-point 10)))
+    (subseq some-path-string (+ chop-point 10))))
+
+(defmethod staple:resolve-source-link (source (page staple:definitions-index-page))
+  (format NIL "http://gitlab.com/Gnuxie/cl-matrix/blob/master/~a#L~a"
+          
+          (strip-to-relative (namestring (getf source :file)))
+    (getf source :row)))
