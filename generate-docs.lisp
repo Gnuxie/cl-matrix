dbcea683483a8186c31f82a54fe0ee1137958f0e
Gnuxie <gnuxie@protonmail.com>
2019-07-28 10:30:34 +0100

    docker image is out of date for ci.

--- generate-docs.lisp
+++ generate-docs.lisp
@@ -1,3 +1,4 @@
+(ql:update-dist "quicklisp" :prompt nil)
 (ql:quickload :staple-markdown)
 (ql:quickload :cl-matrix)
 
@@ -6,5 +7,4 @@
 (staple:generate :cl-matrix :packages '(:cl-matrix :cl-matrix.base-events :matrix-requests)
                  :if-exists ':SUPERSEDE
                  :documents (list (asdf:system-relative-pathname :cl-matrix "README.md"))
-                 :images (list (asdf:system-relative-pathname :cl-matrix "doc/cl-matrix-logo4.png"))
-)
+                 :images (list (asdf:system-relative-pathname :cl-matrix "doc/cl-matrix-logo4.png")))
