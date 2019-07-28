03eb1fefb9326b35f8980842ddd9af50c4f3b7e9
Gnuxie <gnuxie@protonmail.com>
2019-07-28 10:56:55 +0100

    smh

--- generate-docs.lisp
+++ generate-docs.lisp
@@ -4,7 +4,7 @@
 
 (sleep 0.5)
 
-(staple:generate :cl-matrix :packages '(:cl-matrix :cl-matrix.base-events :matrix-requests)
+(staple:generate :cl-matrix :packages '(:cl-matrix :cl-matrix.base-events :cl-matrix.api.client :cl-matrix.api.media :cl-matrix.api.base)
                  :if-exists ':SUPERSEDE
                  :documents (list (asdf:system-relative-pathname :cl-matrix "README.md"))
                  :images (list (asdf:system-relative-pathname :cl-matrix "doc/cl-matrix-logo4.png")))
