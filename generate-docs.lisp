342a9e535424b290350f9b5acd74e61d4b12243b
Gnuxie <gnuxie@protonmail.com>
2019-12-07 21:04:55 +0000

    archive as 0.1.1

--- generate-docs.lisp
+++ generate-docs.lisp
@@ -5,7 +5,7 @@
 (sleep 0.5)
 
 (staple:generate :cl-matrix :packages '(:cl-matrix :cl-matrix.base-events :cl-matrix.api.client :cl-matrix.api.media :cl-matrix.api.base)
-                 :if-exists ':SUPERSEDE
+                 :if-exists :SUPERSEDE
                  :documents (list (asdf:system-relative-pathname :cl-matrix "README.md"))
                  :images (list (asdf:system-relative-pathname :cl-matrix "doc/cl-matrix-logo4.png"))
                  :output-directory (asdf:system-relative-pathname :cl-matrix "doc/"))
